5fe1695c04f9d8af9749db804d06917c31b03312
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-30 18:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function currentLine(line){
 
   for(var i = 0; i < line.length; i++){
     lineNamesandNumbers.push(
-    i + 1 + ". " + line[1]);
+    i + 1 + ". " + line[i]);
   }
   console.log("The line is currently " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(',');
